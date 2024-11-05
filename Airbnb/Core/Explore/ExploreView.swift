639272bdb28429c11d03c88065d2bb0438b5b2b2
i-AryanGupta <aryangupta6008@gmail.com>
2024-11-05 08:25:34 +0530

    Added Exploreview and item view

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,34 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by Yashom on 05/11/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                // why - because we want only the item render on screen as they appear and we don't wanna render all at once
+                LazyVStack(spacing: 32){
+                    
+                    ForEach(0 ... 10, id: \.self){ listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    
+                }
+                .padding()
+            }// scrollview
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
