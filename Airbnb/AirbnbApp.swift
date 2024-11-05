3b3416da5c62a45b4ebf808a8a0180df91cf5adc
i-AryanGupta <aryangupta6008@gmail.com>
2024-11-05 07:05:03 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Yashom on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
